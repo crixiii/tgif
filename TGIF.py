e1d402ba2568f8ff564817f57d987dca9d9d78a4
crixiii <mclarinda.montilla@gmail.com>
2025-08-22 09:48:29 +1200

    updates

--- TGIF.py
+++ TGIF.py
@@ -7,8 +7,6 @@ from astropy.coordinates import SkyCoord
 import matplotlib.animation as animation
 import os
 
-
-
 def nice_plots(change=True):
     plt.rc('font', family='serif')
     fig_width_pt = 244.0
@@ -34,7 +32,7 @@ def mag(flux, zeropoint=20.44):
 class tgif:
     """
     TGIF = T(ESS) GIF (^:
-    Uses a tessreduce object to create a small gif or mp4 of your region of interest!
+    Uses a tessreduce object or a flux file to create a gif or mp4 of your region of interest!
     to-do:
     - implement working on ozstar
     - make mp4 function work better
@@ -44,56 +42,49 @@ class tgif:
     - make multiple gifs/vids for an obs_list
     """
     
-    def __init__(self, trobj=None,flux=None, mjd=None, time_window = 5000, size=90,
-                 detection_list = None, objid=None, ra=None, dec=None, filepath=None, filename=None):
+    def __init__(self, trobj=None,flux=None, ra=None, dec=None, filepath=None, filename=None):
         """
-        trobj: TESSreduce object.
-        mjd: time of the event of interest
-        time_window: the time in seconds to search before and after the specified MJD
-        size: size of the TESSreduce cutout (not currently used)
-        filepath: file path where gif/mp4 is saves
-        filename: name of the gif/mp4
+        Parameters
+        ----------
+        trobj : TESSreduce object.
+        flux : np.ndarray
+            flux cube of [time, y, x]
+        filepath: str
+            location where gif/mp4 is saved
+        filename: str
+            output filename
+        ----------
         """
 
-
-        if flux is not None:
-            self.flux=flux
-        else:
-            self.flux = self.trobj.flux
-
-            
-
-        
-        self.time_window = time_window # in seconds
         self.detection_list = None
-        self.objid = None
         self.event_info = None
-        self.size = size
         self.ra = ra
         self.dec = dec
-        self.detection_list = detection_list
-        self.objid = objid
-        self.detection_list = None
 
         if trobj == None:
             print('-- no tessreduce object given! --')
         else:
             self.trobj = trobj
-            self.detected_inds = np.where(((self.trobj.lc[0] >= (self.mjd - to_days(self.time_window)))) & 
-                                      (self.trobj.lc[0] <=  (self.mjd + to_days(self.time_window))))[0]
+            # self.detected_inds = np.where(((self.trobj.lc[0] >= (self.mjd - to_days(self.time_window)))) & 
+            #                           (self.trobj.lc[0] <=  (self.mjd + to_days(self.time_window))))[0]
         
+        if flux is not None:
+            self.flux=flux
+        else:
+            self.flux = self.trobj.flux
         
+        self.size = len(self.flux)
         self.filename = filename
         self.filepath = filepath
         if self.filepath is None:
             self.filepath = os.getcwd()
 
 
-    def get_event_info(self):
-        """
-        Gets event info (ra, dec, mjd, objid) from TESSELLATE detection csvs.
-        """
-        self.event_info = self.detection_list[self.detection_list['objid']==self.objid]
+    # def get_event_info(self):
+    #     """
+    #     Gets event info (ra, dec, mjd, objid) from TESSELLATE detection csvs.
+    #     """
+    #     self.event_info = self.detection_list[self.detection_list['objid']==self.objid]
 
     
         
@@ -119,6 +110,7 @@ class tgif:
         ani = animation.FuncAnimation(fig, update, frames=frames, interval=interval, blit=True)
 
         plt.show()
+
         if save_gif:
             ani.save(f"{self.filepath}/{self.filename}.gif", writer="pillow", dpi=dpi)
             print(f'saved GIF to {self.filepath}/{self.filename}.gif')
